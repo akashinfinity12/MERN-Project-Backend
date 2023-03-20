75a821286898f7ddb07b6f76cc1280700b7cccfb
Akash <akashudsouza12@gmail.com>
2023-03-20 15:36:49 +0530

    changing port for render deployment

--- Backend/app.js
+++ Backend/app.js
@@ -46,5 +46,5 @@ mongoose
   .connect(
     `mongodb+srv://${process.env.MONGODBUSERNAME}:${process.env.MONGODBPASSWORD}@mern.5gjhp6e.mongodb.net/${process.env.MONGODBNAME}?retryWrites=true&w=majority`
   )
-  .then(() => app.listen(process.env.PORT || 5000))
+  .then(() => app.listen(PORT))
   .catch((error) => console.log(error));
